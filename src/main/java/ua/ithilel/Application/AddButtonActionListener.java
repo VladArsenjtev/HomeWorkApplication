b6932ddb7177a2e20637557b9e955af971a36a87
Vlad Arsenjtev <Vlad.arsenjtev@gmail.com>
2022-06-30 23:30:54 +0300

    Application

---
+++ src/main/java/ua/ithilel/Application/AddButtonActionListener.java
@@ -0,0 +1,24 @@
+package ua.ithilel.Application;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import static ua.ithilel.Application.CalculatorConstants.ERROR_MASSAGE;
+
+public class AddButtonActionListener implements ActionListener {
+
+    private final JTextField input;
+
+    public AddButtonActionListener(JTextField input) {
+        this.input = input;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        if (input.getText().contains(ERROR_MASSAGE)) input.setText("");
+        var actionBtn = (JButton) e.getSource();
+        input.setText(input.getText() + actionBtn.getText());
+    }
+}
--- src/main/java/ua/ithilel/Application/Application.java
+++ src/main/java/ua/ithilel/Application/Application.java
@@ -2,6 +2,6 @@ package ua.ithilel.Application;
 
 public class Application {
     public static void main(String[] args) {
-
+        new BasicApplicationFrame();
     }
 }
